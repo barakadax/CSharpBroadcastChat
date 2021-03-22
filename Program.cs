b4c3d10a891f40d7ab37e1a66778c7ae142727be
Barak <barakadax@gmail.com>
2021-03-22 16:03:30 +0200

    Simple broadcasting chat server.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace broadcastTelnetServer {
+    class Program {
+        static void Main(string[] args) {
+            Socket server = null;
+            IPAddress addr = IPAddress.Parse("127.0.0.1");
+            List<clientObj> clientsList = new List<clientObj>();
+
+            try {
+                server = new Socket(addr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                server.Bind(new IPEndPoint(addr, 9999));
+                server.Listen(100);
+                while (true) {
+                    clientObj client = new clientObj(server.Accept(), clientsList);
+                    clientsList.Add(client);
+                    Thread newThread = new Thread(client.startBroadcasting);
+                    newThread.Start();
+                }
+            }
+            catch {
+                Console.WriteLine("Error acurred server can't run or can't get more connections.");
+            }
+        } // O(∞)
+    }
+}
---
+++ clientObj.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using System.Net.Sockets;
+using System.Collections.Generic;
+
+namespace broadcastTelnetServer {
+    class clientObj {
+        private byte[] buffer;
+        private string myInput;
+        private Socket mySocket;
+        private int amountToReadFromBuffer;
+        private List<clientObj> clientsList;
+
+        public clientObj(Socket socket, List<clientObj> clientsList) {
+            this.myInput = "";
+            this.mySocket = socket;
+            this.buffer = new byte[1500];
+            this.clientsList = clientsList;
+            this.amountToReadFromBuffer = 0;
+            this.mySocket.Send(Encoding.UTF8.GetBytes(" > Telnet 127.0.0.1 9999\n\r > "));
+        } // O(1)
+
+        private bool getMsgToBroadcast() {
+            this.myInput = "";
+            while (!this.myInput.Contains('\n')) {
+                try {
+                    this.amountToReadFromBuffer = this.mySocket.Receive(this.buffer);
+                    this.myInput += Encoding.UTF8.GetString(this.buffer, 0, this.amountToReadFromBuffer);
+                }
+                catch {
+                    this.clientsList.Remove(this);
+                    return true;
+                }
+            }
+            return false;
+        } // O(N)
+
+        private void sendMsg(byte[] msg) {
+            this.mySocket.Send(msg);
+        } // O(1)
+
+        private void sendMsgToOtherClient(clientObj client) {
+            try {
+                client.sendMsg(Encoding.UTF8.GetBytes("\n\r < " + this.myInput + "\r > "));
+            } catch {
+                clientsList.Remove(client);
+                return;
+            }
+        }  // O(N)
+
+        private void whomToSendMsg(clientObj client) {
+            if (client == this)
+                this.mySocket.Send(Encoding.UTF8.GetBytes("\r > "));
+            else
+                this.sendMsgToOtherClient(client);
+        } // O(1)
+
+        public void startBroadcasting() {
+            while (true) {
+                if (this.getMsgToBroadcast())
+                    return;
+                foreach (clientObj client in this.clientsList) {
+                    try {
+                        this.whomToSendMsg(client);
+                    } catch {
+                        continue;
+                    }
+                }
+            }
+        } // O(∞)
+
+        public override string ToString() {
+            return this.mySocket.ToString();
+        } // O(1)
+
+        ~clientObj() {
+            this.mySocket.Close();
+            this.mySocket = null;
+        } // O(1)
+    }
+}
